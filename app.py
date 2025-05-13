625dce44f03afe00163d620c620e7b1a2f664e41
Juan Carlos Rodriguez <78768612+juancarloswill@users.noreply.github.com>
2025-05-12 21:54:16 -0500

    ajuste url_for
    ajuste url_for

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request,session,redirect
+from flask import Flask, render_template, request,session,redirect,url_for
 from pymongo import MongoClient
 from pymongo.server_api import ServerApi
 import os
