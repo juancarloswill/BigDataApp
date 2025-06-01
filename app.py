d384f35a488e4c981dc27c1a2c69fff73feb1a81
Juan Carlos Rodriguez <78768612+juancarloswill@users.noreply.github.com>
2025-06-01 00:20:33 -0500

    arreglo variable
    arreglo variable

--- app.py
+++ app.py
@@ -76,7 +76,7 @@ def login():
             # Verificar credenciales en MongoDB
             user = security_collection.find_one({
                 'usuario': usuario,
-                'password': password
+                'pass': password
             })
             
             if user:
